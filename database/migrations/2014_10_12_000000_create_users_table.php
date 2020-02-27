7987e0a5f048e6b8b22b1e2ed3c168ed6f085d0a
Hoya <angelring127@gmail.com>
2020-02-27 19:46:33 +0900

    Add: set Migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -3,6 +3,7 @@
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
 
 class CreateUsersTable extends Migration
 {
@@ -14,14 +15,19 @@ class CreateUsersTable extends Migration
     public function up()
     {
         Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
-            $table->string('email')->unique();
+            $table->bigIncrements('id')->comment('ユーザーID');
+            $table->string('name')->comment('ユーザー名前');
+            $table->string('email')->unique()->comment('ユーザーメール');
             $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->integer('authority')->comment('ユーザー権限');
+            $table->string('password')->comment('パスワード');
             $table->rememberToken();
             $table->timestamps();
+            $table->softDeletes()->comment('削除状態');
         });
+
+        // テーブルにコメント追加
+        DB::statement("ALTER TABLE users comment 'ユーザー情報'");
     }
 
     /**
